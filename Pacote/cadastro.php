6d14764bf8920cece392fa28c05af22859126ba1
Jjosimar <josimar.infotecnico@gmail.com>
2017-07-14 17:41:55 -0300

    Introdução

---
+++ Pacote/cadastro.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+    
+        <form action="cadastrar.php" method="POST">
+           <input style="display: none;" type="text" value="2" name="tipo" /> 
+           Ano: <input type="text" name="ano">
+           Valor:<input type="text" name="valor">
+           <button type="submit">Enviar</button>
+        </form>
+    </body>
+</html>
